6ea6237307b703c4e0942f89315290a4d7030ac5
suchitadmello <dmellosuchi14@gmail.com>
2019-06-24 17:18:41 -0400

    Added all the sorting algorithm

--- June 19/Assignment-03.py
+++ June 19/Assignment-03.py
@@ -3,54 +3,94 @@
 
 
 def bubble_sort(input_list):
-	bubble_list = input_list
-	temp = []
+	bubble_list = input_list.copy()
 	for i in range(len(bubble_list)):
 		for j in range(0, len(bubble_list)-i-1):
 			if bubble_list[j] > bubble_list[j+1]:
 				bubble_list[j], bubble_list[j+1] = bubble_list[j+1], bubble_list[j]
 	return bubble_list
 
-def quick_sort(lst):
-	quick_list = lst
-    if not quick_list:
-		return []
-    return (quick_sort([x for x in quick_list[1:] if x <  quick_list[0]])
-            + [quick_list[0]] +
-            quick_sort([x for x in quick_list[1:] if x >= quick_list[0]]))
+def partition(quick_list,low,high): 
+    i = ( low-1 )      
+    pivot = quick_list[high]     
+    for j in range(low , high):  
+        if   quick_list[j] <= pivot: 
+            i = i+1 
+            quick_list[i],quick_list[j] = quick_list[j],quick_list[i] 
+    quick_list[i+1],quick_list[high] = quick_list[high],quick_list[i+1] 
+    return ( i+1 )
+
+def quick_sort(lst,low,high):
+    if low < high: 
+        pi = partition(quick_list,low,high) 
+        quick_sort(quick_list, low, pi-1) 
+        quick_sort(quick_list, pi+1, high)
+    return quick_list 
 
 def selection_sort(input_list):
-	selection_list = input_list
+	selection_list = input_list.copy()
 	for i in range(len(selection_list)):  
-		min_idx = i 
+		mid = i 
 		for j in range(i+1, len(selection_list)): 
-			if selection_list[min_idx] > selection_list[j]: 
-				min_idx = j         
-		selection_list[i], selection_list[min_idx] = selection_list[min_idx], selection_list[i]
+			if selection_list[mid] > selection_list[j]: 
+				mid = j         
+		selection_list[i], selection_list[mid] = selection_list[mid], selection_list[i]
 	return selection_list
+
+def merge_sort(merge_list):
+    if len(merge_list) > 1:
+        mid = len(merge_list) // 2
+        left = merge_list[:mid]
+        right = merge_list[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                merge_list[k]=left[i]
+                i=i+1
+            else:
+                merge_list[k]=right[j]
+                j=j+1
+            k=k+1
+
+        while i < len(left):
+            merge_list[k]=left[i]
+            i=i+1
+            k=k+1
+
+        while j < len(right):
+            merge_list[k]=right[j]
+            j=j+1
+            k=k+1
+    return merge_list
 	
 	
-def merge_sort(arr):
-	merge_list = arr
-	mid = int(len(merge_list)/2)
-	print(mid)
-	left_list = merge_list[:mid]
-	right_list = merge_list[mid:]
-	print(left_list)
-	print(right_list)
   
 	
 input_list = [34,56,21,2,4,90,43,23]
 print("Bubble Sort ==> ",bubble_sort(input_list))
-print("Quick Sort ==> ",quick_sort(input_list))
+quick_list = input_list.copy()
+print("Quick Sort ==> ",quick_sort(quick_list,0,len(input_list)-1))
 print("Selection Sort ==> ",selection_sort(input_list))
-print("Merge Sort ==> ",merge_sort(input_list))
+merge_list = input_list.copy()
+print("Merge Sort ==> ",merge_sort(merge_list))
 			
 		
 			
-			
-			
-#Question 1			
+		
+#Question 1	
+
+def string_sort(lst):
+    if not lst:
+	    return []
+    return (string_sort([x for x in lst[1:] if x <  lst[0]])
+            + [lst[0]] +
+            string_sort([x for x in lst[1:] if x >= lst[0]]))
+
 input_string = input("\nEnter string: ")
 words_list = []
 word = ''
@@ -62,10 +102,10 @@ for index,ch in enumerate(input_string):
 		word = word + ch
 words_list.append(word)
 print("Output A) ==> ",words_list)
-sorted_word_list = quick_sort(words_list)
+sorted_word_list = string_sort(words_list)
 sorted_list = []
 print("Output B) ==> ",sorted_word_list)
 for word in sorted_word_list:
-	sorted_list.append(''.join(quick_sort(word)))
+	sorted_list.append(''.join(string_sort(word)))
 print("Output C) ==> ",sorted_list)
 		
