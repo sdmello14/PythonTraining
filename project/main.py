c1b368c3ec2c4e51dcc9d6fd4ab2eda04cddc830
suchitadmello <dmellosuchi14@gmail.com>
2019-07-12 16:05:20 -0400

    added about and gallery page

--- project/main.py
+++ project/main.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/home")
+@app.route("/about")
 def home():
-    return render_template('home.html')
+    return render_template('about.html')
+
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
